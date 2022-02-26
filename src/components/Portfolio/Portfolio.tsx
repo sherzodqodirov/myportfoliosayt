649265bfe061a7319e23bfb6565f1414ec7d5f59
ahrorbeksulaymanov <78092308+ahrorbeksulaymanov@users.noreply.github.com>
2022-02-26 13:14:57 +0500

    new project is added

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -145,6 +145,34 @@ export function Portfolio() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://github.com/ahrorbeksulaymanov/currency-live" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
+                <a href="https://ahrorbeksulaymanov.github.io/currency-live" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <a style={{ color: "#fff" }} href="https://ahrorbeksulaymanov.github.io/currency-live" target="_blank" rel="noreferrer">
+                <h3>Currencies app</h3>
+              </a>
+              <p>I created this site for my portfolio. You can see the real Currencies through this app. Currency information on this site is updated every minute</p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
       </div>
     </Container>
   );
