f158a532fad0b830b8690981ca609a4900542d10
ahrorbeksulaymanov <78092308+ahrorbeksulaymanov@users.noreply.github.com>
2022-03-10 22:11:52 +0500

    a project is added

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -28,6 +28,13 @@ export function Portfolio() {
               <p>This system is a driving school system. This platform is a great convenience for school administration and for school students. I was involved in the preparation of the front end part of this platform</p>
             </div>
             <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
+              </ul>
               <ul className="tech-list">
                 <li>JavaScript</li>
                 <li>React js</li>
@@ -54,32 +61,13 @@ export function Portfolio() {
               <p>This system allows people to get legal advice for free and online. I was involved in the preparation of the front end part of this site.</p>
             </div>
             <footer>
-              <ul className="tech-list">
-                <li>JavaScript</li>
-                <li>React js</li>
-                <li>Redux</li>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
               </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              <div className="project-links">
-                <a href="http://reception.tsul.uz/login" target="_blank" rel="noreferrer">
-                  <img src={externalLinkIcon} alt="Visitar site" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <a style={{ color: "#fff" }} href="http://reception.tsul.uz/login" target="_blank" rel="noreferrer">
-                <h3>Reseption TSUL</h3>
-              </a>
-              <p>This system is designed for the reception of the Tashkent State University of Law . I was involved in the preparation of the front of this site.</p>
-            </div>
-            <footer>
               <ul className="tech-list">
                 <li>JavaScript</li>
                 <li>React js</li>
@@ -109,9 +97,84 @@ export function Portfolio() {
               <p>I created this site for my portfolio.</p>
             </div>
             <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
+              </ul>
               <ul className="tech-list">
                 <li>JavaScript</li>
                 <li>Next js</li>
+                <li>Redux</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://github.com/ahrorbeksulaymanov/luxury-hotel" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
+                <a href="https://ahrorbeksulaymanov.github.io/luxury-hotel" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <a style={{ color: "#fff" }} href="https://ahrorbeksulaymanov.github.io/luxury-hotel" target="_blank" rel="noreferrer">
+                <h3>Luxury hotel</h3>
+              </a>
+              <p>I created this site for my portfolio.</p>
+            </div>
+            <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
+              </ul>
+              <ul className="tech-list">
+                <li>JavaScript</li>
+                <li>React js</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="http://reception.tsul.uz/login" target="_blank" rel="noreferrer">
+                  <img src={externalLinkIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <a style={{ color: "#fff" }} href="http://reception.tsul.uz/login" target="_blank" rel="noreferrer">
+                <h3>Reseption TSUL</h3>
+              </a>
+              <p>This system is designed for the reception of the Tashkent State University of Law . I was involved in the preparation of the front of this site.</p>
+            </div>
+            <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
+              </ul>
+              <ul className="tech-list">
+                <li>JavaScript</li>
+                <li>React js</li>
                 <li>Redux</li>
               </ul>
             </footer>
@@ -138,6 +201,13 @@ export function Portfolio() {
               <p>I created this site for my portfolio. You can see the real weather through this app</p>
             </div>
             <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
+              </ul>
               <ul className="tech-list">
                 <li>JavaScript</li>
                 <li>TypeScript</li>
@@ -167,12 +237,20 @@ export function Portfolio() {
             </div>
             <footer>
               <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+                <li>Antd design</li>
+              </ul>
+              <ul className="tech-list">
                 <li>JavaScript</li>
                 <li>TypeScript</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
+
       </div>
     </Container>
   );
